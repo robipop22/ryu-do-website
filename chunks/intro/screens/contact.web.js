8f61a0bdceae17d535afdf1bae15b183bd3fde1e
Robert Pop <poprobertdaniel22@gmail.com>
2018-12-03 23:07:32 +0200

    prepared map component

--- chunks/intro/screens/contact.web.js
+++ chunks/intro/screens/contact.web.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Screen } from 'react-dom-chunky'
+import * as CustomComponents from '../components'
 
 export default class ContactScreen extends Screen {
   constructor(props) {
@@ -9,6 +10,13 @@ export default class ContactScreen extends Screen {
 
   componentDidMount() {
     super.componentDidMount()
+  }
+
+  loadCustomComponent(props) {
+    return props => {
+      const CustomComponent = CustomComponents[props.source]
+      return <CustomComponent {...props} />
+    }
   }
 
   components() {
